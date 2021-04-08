346c404182ac87561505bae82efec6152cd0b6d3
huanggengzhong <915835528@qq.com>
2021-04-08 23:57:41 +0800

    feat:适配器添加

--- lib/http/core/hi_net.dart
+++ lib/http/core/hi_net.dart
@@ -1,5 +1,8 @@
 import 'package:bilibili_app/http/request/base_request.dart';
 
+import 'hi_error.dart';
+import 'hi_net_adapter.dart';
+
 class HiNet {
 //  创建命名构造单例
   HiNet._();
@@ -15,9 +18,45 @@ class HiNet {
 
 //fire请求
   Future fire(BaseRequest request) async {
-    var response = await send(request);
-    var result = response['data'];
+    HiNetResponse response;
+    var error;
+
+    //捕获异常
+    try {
+      response = await send(request);
+    } on HiNetError catch (e) {
+      //HiNetError类型的异常
+      error = e;
+      response = e.data;
+      prinlog(e.message);
+    } catch (e) {
+      //  进入这里代码有其它异常
+      error = e;
+      prinlog(e);
+    }
+    //如果响应体为空,打印异常
+    if (response == null) {
+      prinlog(error);
+    }
+
+    var result = response.data;
     print("result:$result");
+    //根据不同的错误状态码返回封装好的异常类
+    var status = response.statusCode;
+    switch (status) {
+      case 200:
+        return result;
+        break;
+      case 401:
+        throw NeedLogin();
+        break;
+      case 403:
+        throw NeedAuth(result.toString(), data: result);
+        break;
+      default:
+        throw HiNetError(status, result.toString(), data: result);
+        break;
+    }
     return result;
   }
 
@@ -27,10 +66,11 @@ class HiNet {
     prinlog("method:${request.httpMethod()}");
     request.addHeader("token", "123");
     prinlog("header:${request.header}");
-    return Future.value({
-      "statusCode": 200,
-      "data": {"code": 0, "message": "success"}
-    });
+    // return Future.value({
+    //   "statusCode": 200,
+    //   "data": {"code": 0, "message": "success"}
+    // });
+    //  使用HiNetAdapter模拟数据请求
   }
 
 //  统一打印方法
---
+++ lib/http/core/mock_adapter.dart
@@ -0,0 +1,12 @@
+import 'package:bilibili_app/http/core/hi_net_adapter.dart';
+import 'package:bilibili_app/http/request/base_request.dart';
+
+class MockAdaptor extends HiNetAdaper {
+  @override
+  Future<HiNetResponse<T>> send<T>(BaseRequest request) {
+    return Future<HiNetResponse>.delayed(Duration(milliseconds: 1000), () {
+      return HiNetResponse(
+          data: {"code": 0, "message": "success", "statusCode": 200});
+    });
+  }
+}
